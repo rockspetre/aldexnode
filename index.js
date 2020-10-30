d5180f1f88d52edd77240268deb00a185be45551
Ugochukwu Nwajagu <ugochukwu.nwajagu@andela.com>
2020-10-30 09:40:05 +0100

    removed firebase

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express  =require('express');
 var app = express();
 var cors = require('cors')
-var firebase = require('firebase');
 app.use(express.json());
 var bodyParser = require('body-parser');
 var tweetservcice  = require('./tweetsservice');
@@ -10,17 +9,6 @@ var trends = require('./trends');
 var fbase = require('./fbase');
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
-var firebaseConfig = { 
-    apiKey: "AIzaSyAkA7vDIs0UJitojpt9OZ5oQNlpHbACaHE",
-    authDomain: "makemeup-2bf71.firebaseapp.com",
-    databaseURL: "https://makemeup-2bf71.firebaseio.com",
-    projectId: "makemeup-2bf71",
-    storageBucket: "makemeup-2bf71.appspot.com",
-    messagingSenderId: "536766362231",
-    appId: "1:536766362231:web:73495be2e9f72dab11cc72"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 
 
 app.get('/getTweetsByHashTag',(req,res)=>{
